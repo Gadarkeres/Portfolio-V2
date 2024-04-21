d4d98dd5fbd2cdf1194cfa537f11e48c8c8cd970
Matheus Rafael <matheusrafael77@hotmail.com>
2024-04-20 22:07:17 -0300

    ajuste export const

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Roboto } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header/header';
 
-export const roboto = Roboto({ weight:'400'});
+const roboto = Roboto({ weight:'400', subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Portfolio - Matheus',
