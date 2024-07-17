00f22f3517e715729acbc58e3f74126f66729583
Matheus Rafael <matheusrafael77@hotmail.com>
2024-07-16 22:09:11 -0300

    fix²

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Apresentation from "../components/apresentation/apresentation";
+import Apresentation from "@/components/Apresentation/apresentation";
 
 export default function Home() {
   return (
     <main>
-      <Apresentation />
+      <Apresentation/>
     </main>
   );
 }
