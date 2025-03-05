1f2035dc5f44b981931982468bb915ea40046081
Matheus Rafael <matheusrafael77@hotmail.com>
2025-03-05 11:52:30 -0300

    feat: adiciona novos certificados

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -7,6 +7,9 @@ import course06 from "../assets/certificates/react-desafios.png";
 import course07 from "../assets/certificates/Stream-java.png";
 import course08 from "../assets/certificates/persistencia-de-dados-jpa.png";
 import course09 from "../assets/certificates/sql-course.png";
+import course10 from "../assets/certificates/controle-excecoes-java-1.png"
+import course11 from "../assets/certificates/docker-1.png"
+import course12 from "../assets/certificates/documente-teste-api-course-1.png"
 
 export interface CertificateData {
   img: any;
@@ -48,6 +51,18 @@ export const certificates: CertificateData[] = [
   },
   {
     img: course09,
-    alt:"curso de sql"
-  }
+    alt:"Curso de sql"
+  },
+  {
+    img: course10,
+    alt:"Controle e personalize exceções no java"
+  },
+  {
+    img: course11,
+    alt:"Curso de Docker"
+  },
+  {
+    img: course12,
+    alt:"Teste e documente uma api com swagger"
+  },
 ];
