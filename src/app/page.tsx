0eec6a819309273f75813345240ca1aafead4947
Matheus Rafael <matheusrafael77@hotmail.com>
2025-10-26 17:53:00 -0300

    fix(imports) ajusta import de apresentarion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Apresentation from "@/components/Apresentation/apresentation";
+import Apresentation from "@/components/Apresentation/Apresentation";
 
 export default function Home() {
   return (
